59292491c77cfd0955e0ffcc118b80861a7e7b66
JeongRon <wjdfhs@gmail.com>
2023-03-25 22:50:23 +0900

    명령어 최적화 진행 중

--- sort.c
+++ sort.c
@@ -6,91 +6,89 @@
 /*   By: jeongrol <jeongrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:17:43 by jeongrol          #+#    #+#             */
-/*   Updated: 2023/03/23 19:37:18 by jeongrol         ###   ########.fr       */
+/*   Updated: 2023/03/25 22:50:00 by jeongrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// 반환값 (0이면,바로 pb) (-1이면, ra + pb) (1이면, rra + pb) 
-int	find_least_command(t_stack **stack_a, t_info *info)
+void	run_ra_command(t_stack **stack_a, t_stack **stack_b, t_info *info)
 {
-	t_stack		*tmp;
-	int			cnt;
-	int			ra_cnt;
-	int			rra_cnt;
-
-	tmp = *stack_a;
-	cnt = 0;
-	ra_cnt = 0;
-	rra_cnt = 0;
-	if (info->order_start <= tmp->order && tmp->order <= info->order_end)
-		return (0);
-	while (tmp)
+	while (1)
 	{
-		if (info->order_start <= tmp->order && tmp->order <= info->order_end)
+		if (info->order_start <= (*stack_a)->order && (*stack_a)->order <= info->order_end)
 		{
-			if (ra_cnt == 0)
-				ra_cnt = cnt;
-			else
-				rra_cnt = cnt;
+			pb(stack_a, stack_b);
+			break ;
 		}
-		cnt++;
-		tmp = tmp->next;
-	}
-	rra_cnt = ft_lstsize(*stack_a) - rra_cnt;
-	if (ra_cnt <= rra_cnt)
-		return (-1);
-	return (1);
-}
-
-void	run_command(int flag, t_stack **stack_a, t_stack **stack_b, t_info *info)
-{
-	if (flag == 0)
-		pb(stack_a, stack_b);
-	else if (flag == -1)
-	{
-		while (1)
-		{
-			ra(stack_a);
-			if (info->order_start <= (*stack_a)->order && (*stack_a)->order <= info->order_end)
-			{
-				pb(stack_a, stack_b);
-				break ;
-			}
-		}
-	}
-	else
-	{
-		while (1)
-		{
-			rra(stack_a);
-			if (info->order_start <= (*stack_a)->order && (*stack_a)->order <= info->order_end)
-			{
-				pb(stack_a, stack_b);
-				break ;
-			}
-		}
+		ra(stack_a, 0);
 	}
 	if (info->order_center > (*stack_b)->order)
-		rb(stack_b);
+	{
+		if (info->play_cnt >= 2 && (info->order_start > (*stack_a)->order || (*stack_a)->order > info->order_end))
+			rr(stack_a, stack_b, 1);
+		else
+			rb(stack_b, 0);
+	}
+}
+
+void	run_rra_command(t_stack **stack_a, t_stack **stack_b, t_info *info)
+{
+	while (1)
+	{
+		if (info->order_start <= (*stack_a)->order && (*stack_a)->order <= info->order_end)
+		{
+			pb(stack_a, stack_b);
+			break ;
+		}
+		rra(stack_a, 0);
+	}
+	if (info->order_center > (*stack_b)->order)
+	{
+		rb(stack_b, 0);
+	}
+}
+
+// 최적화 방법 - 3분의 2 이상 지점에 분포가 많을시? 
+int	choose_direction(t_stack **stack_a, t_info *info)
+{
+	t_stack		*tmp;
+	int			standard;
+	int			node;
+	int			cnt;
+
+	tmp = *stack_a;
+	standard = ft_lstsize(*stack_a) / 2;
+	node = 1;
+	cnt = 0;
+	while (tmp)
+	{
+		if (standard <= node)
+			if (info->order_start <= tmp->order && tmp->order <= info->order_end)
+				cnt++;
+		tmp = tmp->next;
+	}
+	if (cnt >= (info->play_cnt * 2) / 3)
+		return (-1);
+	return (1);
 }
 
 // a->b 스택으로 보내기 
 void	send_a_to_b(t_stack **stack_a, t_stack **stack_b, t_info *info)
 {
-	int		flag;
+	int	dir;
 
-	flag = 0;
+	dir = 0;
 	// section 순으로 보내기
 	while (info->section_all >= info->play_section)
 	{
+		dir = choose_direction(stack_a, info);
 		while (info->play_cnt != 0)
 		{
-			// ra, rra 중 최단 명령어 찾기
-			flag = find_least_command(stack_a, info);
-			// 최단 명령어로 실제 동작 (ra || rra) + (pb)
-			run_command(flag, stack_a, stack_b, info);
+			if (dir == 1)
+				run_ra_command(stack_a, stack_b, info);
+			else
+				run_rra_command(stack_a, stack_b, info);
 			info->play_cnt--;
 		}
 		// section 1증가 -> 다른 값들도 갱신
@@ -132,15 +130,15 @@ int	find_least_com(int find_order, t_stack **stack_b)
 		return (1);
 }
 
-void	run_com(int flag, int find_order, t_stack **stack_a, t_stack **stack_b)
+void	run_com(int direction, int find_order, t_stack **stack_a, t_stack **stack_b)
 {
-	if (flag == 0)
+	if (direction == 0)
 		pa(stack_a, stack_b);
-	else if (flag == -1)
+	else if (direction == -1)
 	{
 		while (1)
 		{
-			rb(stack_b);
+			rb(stack_b, 0);
 			if ((*stack_b)->order == find_order)
 			{
 				pa(stack_a, stack_b);
@@ -152,7 +150,7 @@ void	run_com(int flag, int find_order, t_stack **stack_a, t_stack **stack_b)
 	{
 		while (1)
 		{
-			rrb(stack_b);
+			rrb(stack_b, 0);
 			if ((*stack_b)->order == find_order)
 			{
 				pa(stack_a, stack_b);
@@ -166,14 +164,14 @@ void	run_com(int flag, int find_order, t_stack **stack_a, t_stack **stack_b)
 void	send_b_to_a(t_stack **stack_a, t_stack **stack_b, t_info *info)
 {
 	int		find_order;
-	int		flag;
+	int		direction;
 
-	flag = 0;
+	direction = 0;
 	find_order = info->length;
 	while (find_order != 0)
 	{
-		flag = find_least_com(find_order, stack_b);
-		run_com(flag, find_order, stack_a, stack_b);
+		direction = find_least_com(find_order, stack_b);
+		run_com(direction, find_order, stack_a, stack_b);
 		find_order--;
 	}
 }
